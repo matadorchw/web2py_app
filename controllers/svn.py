68da779bd3007bf89d1fbeb31c08c7dca87dd742
truman <2290350531@qq.com>
2021-07-30 15:42:52 +0800

    zh-cn

--- controllers/svn.py
+++ controllers/svn.py
@@ -7,25 +7,30 @@ def show_users():
     return dict(title=T('Show SVN Users'), users=h.get_users())
 
 
+@auth.requires_login()
 def create_user():
-    return dict(title='Create SVN User')
+    return dict(title=T('Create SVN User'))
 
 
+@auth.requires_login()
 def set_password():
     response.view = 'svn/create_user.html'
-    return dict(title='Set Password', user_name=request.args[0])
+    return dict(title=T('Set Password'), user_name=request.args[0])
 
 
+@auth.requires_login()
 def set_password_done():
     h.set_password(request.vars['name'], request.vars['password'])
     redirect(URL(c='svn', f='show_users'))
 
 
+@auth.requires_login()
 def delete_user():
     h.delete_user(request.args[0])
     redirect(URL(c='svn', f='show_users'))
 
 
+@auth.requires_login()
 def create_user_done():
     h.create_user(request.vars['name'], request.vars['password'])
     redirect(URL(c='svn', f='show_users'))
--- models/dbs.py
+++ models/dbs.py
@@ -1,4 +1,4 @@
-# db.auth_user._format = "%(first_name)s"
-# auth.settings.actions_disabled = ['register']
+db.auth_user._format = "%(first_name)s"
+auth.settings.actions_disabled = ['register']
 
 
