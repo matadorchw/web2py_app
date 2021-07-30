b3e96955f358a165108e3e8d8120525903ab437c
truman <2290350531@qq.com>
2021-07-30 18:20:18 +0800

    pythoncom.CoInitialize

--- modules/svnhelper.py
+++ modules/svnhelper.py
@@ -1,37 +1,54 @@
 import wmi
+import pythoncom
 
 
 class VisualSvnHelper:
     def __init__(self, computer='', user='', password=''):
+        pythoncom.CoInitialize()
         self.ns = wmi.WMI(namespace='root/VisualSVN')
         self.user = self.ns.get('VisualSVN_User')
         self.group = self.ns.get('VisualSVN_Group')
+        pythoncom.CoUninitialize()
 
     def get_users(self):
-        return [user.Name for user in self.ns.instances('VisualSVN_User')]
+        pythoncom.CoInitialize()
+        result = [user.Name for user in self.ns.instances('VisualSVN_User')]
+        pythoncom.CoUninitialize()
+        return result
 
     def create_user(self, name, password):
+        pythoncom.CoInitialize()
         self.user.Create(name, password)
+        pythoncom.CoUninitialize()
 
     def set_password(self, name, password):
+        pythoncom.CoInitialize()
         for user in self.ns.instances('VisualSVN_User'):
             if user.Name == name:
                 user.SetPassword(password)
                 break
+        pythoncom.CoUninitialize()
 
     def delete_user(self, name):
+        pythoncom.CoInitialize()
         self.user.Delete(name)
+        pythoncom.CoUninitialize()
 
     def get_groups(self):
-        return [group.Name for group in self.ns.instances('VisualSVN_Group')]
+        pythoncom.CoInitialize()
+        result = [group.Name for group in self.ns.instances('VisualSVN_Group')]
+        pythoncom.CoUninitialize()
+        return result
 
     def get_group_members(self, group):
+        pythoncom.CoInitialize()
         members = []
         for g in self.ns.instances('VisualSVN_Group'):
             if g.Name == group:
                 for m in g.GetMembers()[0]:
                     members.append(m.Name)
                 break
+        pythoncom.CoUninitialize()
         return members
 
 
